5008e76fc22ba7911906d21d99202cd1c8ce0651
chriscoliveira <christian.coliveira@gmail.com>
2022-09-14 18:46:06 -0300

    inicio

---
+++ previsao.py
@@ -0,0 +1,8 @@
+# instalar yfinance prophet pandas plotly cython fbprophet
+
+import pandas
+import yfinance
+from datetime import datetime, timedelta
+import plotly.graph_objects
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly, plot_components_plotly
